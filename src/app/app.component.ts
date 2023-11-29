bbc06e0a3959da7ec381d8e9077dede4e6a05c9c
alexjames4 <alexanderjamesmansfield@gmail.com>
2023-11-29 14:14:21 +0000

    page now displays correctly. css tweaking required

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoapp16';
+  readonly APIUrl = "http://localhost:5057/api/todoapp/";
+
+  constructor(private http:HttpClient){
+  }
+  notes:any=[];
+
+  refreshNotes(){
+    this.http.get(this.APIUrl+'GetNotes').subscribe(data=>{
+      this.notes=data;
+    })
+  }
+
+  ngOnInit(){
+    this.refreshNotes();
+  }
+
+  addNotes(){
+    var newNotes=(<HTMLInputElement>document.getElementById("newNotes")).value;
+    var formData=new FormData();
+    formData.append("newNotes", newNotes);
+    this.http.post(this.APIUrl+'AddNotes',formData).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+    })
+  }
+
+  deleteNotes(id:any){
+    this.http.delete(this.APIUrl+'DeleteNotes?id='+id).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+    })
+  }
 }
